651e0b7c5e5767f0e4adc90c69defa9b566d475e
dontdoitwalter <emailwaltervanhooser@gmail.com>
2018-11-16 17:15:20 -0500

    another commit again

--- controllers/storycontroller.js
+++ controllers/storycontroller.js
@@ -6,7 +6,6 @@ let UserStory = sequelize.import('../models/story');
 
 router.post('/story', validateSesh, function(req, res){
     let Story = req.body.storysubmit.story;
-
     UserStory.create({
         story:Story
     }).then(
@@ -19,5 +18,18 @@ router.post('/story', validateSesh, function(req, res){
     function createError(err){
         res.send(500, err.message)
     }
+})
+router.get('/read', validateSesh, function(req, res){
+    let Story=req.body.storysubmit.story
+    Story.findAll({
+        
+    }).then(
+        function getSuccess(story){
+            res.json(story)
+        },
+        function getError(err){
+            res.send(500,err.message)
+        }
+    )
 })
 module.exports = router;
--- controllers/suggestcontroller.js
+++
@@ -1,6 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let sequelize = require('../db');
-const validateSesh = require('../middleware/validatesession')
-let UserSuggestion = sequelize.import('../models/suggest');
-
